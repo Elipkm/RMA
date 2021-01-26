868c0bf6660e1c945843df7119c309a0d326aa1e
Elias <elias.erkinger@htlpinkafeld.at>
2021-01-26 13:52:43 +0100

    Restful-Service / Enabling CORS for testing purposes

--- RMA-backend-java/src/main/java/at/htlpinkafeld/RMA_backend_java/AuthTest.java
+++ RMA-backend-java/src/main/java/at/htlpinkafeld/RMA_backend_java/AuthTest.java
@@ -5,12 +5,34 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MultivaluedMap;
+import java.io.IOException;
+import java.util.Collections;
+
 @Configuration
 @EnableWebSecurity
 public class AuthTest extends WebSecurityConfigurerAdapter {
+
+    public AuthTest(){
+   /*     ClientRequestFilter tokenHeadersAdder = new ClientRequestFilter()
+        {
+            public void filter(ClientRequestContext ctx) throws IOException
+            {
+                final MultivaluedMap<String, Object> headers = ctx.getHeaders();
+                headers.put("X-MyKey1", Collections.singletonList("MyValue1"));
+            }
+        };
+
+        javax.ws.rs.core.Configuration clientConfig = new;
+        Client client = ClientBuilder.newClient(clientConfig);
+        client.register(tokenHeadersAdder);
+        WebTarget webTarget = client.target("");  */
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
     }
+
 }
