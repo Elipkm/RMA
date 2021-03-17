4520cb7722c75e0b4015b8d62ec8853d246c30c7
Daniel Mader <daniel.mader@htlpinkafeld.at>
2021-03-17 12:54:15 +0100

    Restful-Service / added Token-Expiration feature

--- RMA-backend-java/src/main/java/at/htlpinkafeld/RMA_backend_java/servlet/authentication/TokenJwts.java
+++ RMA-backend-java/src/main/java/at/htlpinkafeld/RMA_backend_java/servlet/authentication/TokenJwts.java
@@ -1,9 +1,11 @@
 package at.htlpinkafeld.RMA_backend_java.servlet.authentication;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
+import java.util.Date;
 
 public class TokenJwts implements TokenGenerator, TokenProcessor {
     private final Key key;
@@ -17,16 +19,23 @@ public class TokenJwts implements TokenGenerator, TokenProcessor {
 
     @Override
     public Token issueToken(String username){
-        String token = Jwts.builder().setSubject(username).signWith(this.key).compact();
+        final int expTimeInMins = 120;
+        final int expTimeInMills = expTimeInMins * 60000;
+
+        Date expDate = new Date(System.currentTimeMillis() + expTimeInMills);
+
+        String token = Jwts.builder().setSubject(username).setExpiration(expDate).signWith(this.key).compact();
         return new Token(token);
     }
 
     @Override
-    public Token validate(String token) throws InvalidTokenException {
+    public Token validate(String token) throws InvalidTokenException, ExpiredJwtException {
         try {
             Jwts.parserBuilder().setSigningKey(this.key).build().parseClaimsJws(token);
 
             return new Token(token);
+        } catch (ExpiredJwtException e) {
+            throw e;
         }catch (Exception exception){
             throw (InvalidTokenException) new InvalidTokenException().initCause(exception);
         }
